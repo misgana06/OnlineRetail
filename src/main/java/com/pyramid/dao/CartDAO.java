535dab346658138d0bdbf38cbb4faa2be95c18d1
Fred <mtezera@gmail.com>
2019-12-10 13:55:31 -0500

    Cart & cartDAO

--- src/main/java/com/pyramid/dao/CartDAO.java
+++ src/main/java/com/pyramid/dao/CartDAO.java
@@ -1,5 +1,50 @@
 package com.pyramid.dao;
 
-public class CartDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.pyramid.model.Cart;
+
+public class CartDAO {
+	
+	private JdbcTemplate jt;
+	
+	public CartDAO(JdbcTemplate jt) {
+		super();
+		this.jt = jt;
+	}
+
+	public JdbcTemplate getJt() {
+		return jt;
+	}
+
+	public void setJt(JdbcTemplate jt) {
+		this.jt = jt;
+	}
+	
+	public int clearCartDetails() {
+		return jt.update("truncate Cart");
+	}
+	
+	public int addCartDetails(Cart c) {
+		return jt.update("insert into cart (p_name,p_qty,total_p_price) values('"+c.getP_name()+"',"+c.getP_qty()+","+c.getTotal_p_price()+")");
+	}
+	
+	
+	public List<Cart> viewCartDetails(){
+		return jt.query("select * from supplier" , new RowMapper<Cart>() {
+			public Cart mapRow(ResultSet r, int n) throws SQLException {
+				Cart s = new Cart();
+				s.setP_name(r.getString(2));
+				s.setP_qty(r.getDouble(3));
+				s.setTotal_p_price(r.getDouble(4));
+				return s;
+			}
+		});
+
+	}
 }
--- src/main/java/com/pyramid/model/Cart.java
+++ src/main/java/com/pyramid/model/Cart.java
@@ -1,5 +1,40 @@
 package com.pyramid.model;
 
 public class Cart {
+	
+	private String p_name;
+	private double p_qty;
+	private double total_p_price;
+	
+	
+	public Cart() {
+		
+	}
+	public Cart(String p_name, double p_qty, double total_p_price) {
+		super();
+		this.p_name = p_name;
+		this.p_qty = p_qty;
+		this.total_p_price = total_p_price;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	public double getP_qty() {
+		return p_qty;
+	}
+	public void setP_qty(double p_qty) {
+		this.p_qty = p_qty;
+	}
+	public double getTotal_p_price() {
+		return total_p_price;
+	}
+	public void setTotal_p_price(double total_p_price) {
+		this.total_p_price = total_p_price;
+	}
+	
+	
 
 }
